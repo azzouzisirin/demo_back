dcfaada61e7a7ccb4b1609a29db8cdd844bbe27f
Sirine AZZOUZI <sirineazzouzi5@gmail.com>
2023-06-19 14:33:58 +0100

    Add existing prles to Git

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const app=express()
 app.get('/',(req,res)=>{
     res.send('hello world')
 })
+
+app.listen(8000, () => {
+    console.log(`Server is running on port 8000.`);
+    const os = require('os');
+});
